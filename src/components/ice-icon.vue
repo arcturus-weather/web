0ba889b654f2de5202093f4c1f130745667d27ed
ARCTURUS <1638330246@qq.com>
2022-03-21 13:28:57 +0800

    封装的 ICON 组件

---
+++ src/components/ice-icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg aria-hidden="true" :style="styleObject">
+    <use :xlink:href="`#${name}`"></use>
+  </svg>
+</template>
+
+<script>
+/*************************
+ *  引入 iconfont 图标    *
+ *************************/
+import '//at.alicdn.com/t/font_3222523_qxdxwmlxjn.js';
+import '//at.alicdn.com/t/font_3192173_1er6fiyhl1p.js';
+import '//at.alicdn.com/t/font_3184399_x1h5png7n8.js';
+import '//at.alicdn.com/t/font_3185244_88v7v16rhan.js';
+
+export default {
+  name: "ice-icon",
+  props: {
+    name: String,
+    size: {
+      default: 50,
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      styleObject: {
+        width: `${this.size}px`,
+        height: `${this.size}px`,
+      },
+    };
+  },
+};
+</script>
