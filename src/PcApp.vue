8dc91b1dc94d5c982a1bc2f876342ff69f80c3ea
ARCTURUS <1638330246@qq.com>
2022-05-24 22:49:49 +0800

    feat: other changes

--- src/PcApp.vue
+++ src/PcApp.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from 'components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import PcApp from '@/PcApp.vue'
+import vuetify from 'plugins/vuetify'
+import { loadFonts } from 'plugins/webfontloader'
+import i18n from 'i18n'
 
 loadFonts()
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(PcApp).use(i18n).use(vuetify).mount('#app')
