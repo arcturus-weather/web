cfb80c3243cdce92276a28f875c047e01b1c818c
ARCTURUS <1638330246@qq.com>
2022-05-24 22:50:13 +0800

    feat: vuetify 绑定指令和组件

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,6 +5,11 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  components,
+  directives,
+})
