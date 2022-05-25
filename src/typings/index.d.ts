eb25150f9df05f18db8f7ac0e504583661d50d28
ARCTURUS <1638330246@qq.com>
2022-05-25 13:23:07 +0800

    feat: add declare for some modules

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,3 +1,16 @@
 declare module 'i18n'
+declare module 'utils/uuid'
+declare module 'utils/location'
+declare module 'weather/models/air'
+declare module 'weather/models/astronomy'
+declare module 'weather/models/warnings'
+declare module 'weather/models/livingIndex'
+declare module 'weather/models/precipitation'
+declare module 'weather/models/weatherItem'
 declare module 'plugins/vuetify'
 declare module 'plugins/webfontloader'
+
+interface Date {
+  format(fmt: string): string
+  deltaDay(n: number): Date
+}
