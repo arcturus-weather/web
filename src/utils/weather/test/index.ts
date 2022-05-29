05647cd36d8cfa883f7e934d94022bc3815e4b7e
ARCTURUS <1638330246@qq.com>
2022-05-29 15:23:37 +0800

    test: 天气接口测试用例

---
+++ src/utils/weather/test/index.ts
@@ -0,0 +1,23 @@
+// 用于测试天气接口, 直接在 App.vue 中引入即可
+
+import QWeatherStrategies from '../strategies/qweather';
+import Weather from '../strategies/weather';
+import { qWeatherKey, openWeatherKey } from 'src/appKey';
+import OpenWeatherStrategies from '../strategies/openweather';
+import Location from 'utils/location/location';
+
+const qw = new QWeatherStrategies(qWeatherKey);
+const ow = new OpenWeatherStrategies(openWeatherKey);
+
+const w = new Weather(qw);
+
+const loc = new Location({
+  latitude: 21,
+  longitude: 110,
+});
+
+w.getAllweather(loc).then((res) => console.log(res));
+
+w.strategy = ow; // 切换策略
+
+w.getAllweather(loc).then((res) => console.log(res));
