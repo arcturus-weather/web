46fc21d549ee748b0d1646dd4784a7bfa3e02e6c
ARCTURUS <1638330246@qq.com>
2022-05-29 15:25:46 +0800

    feat: 位置相关类

--- src/utils/location/location.ts
+++ src/utils/location/location.ts
@@ -1,30 +1,30 @@
-export default class Location {
+export default class Location implements ILocation {
   constructor(
     private options: {
-      latitude: number
-      longitude: number
-      city?: string
-      address?: string
+      latitude: number;
+      longitude: number;
+      city?: string;
+      address?: string;
     }
   ) {}
 
   get latitude(): number {
-    return this.options.latitude
+    return this.options.latitude;
   }
 
   get longitude(): number {
-    return this.options.longitude
+    return this.options.longitude;
   }
 
   get city(): string | undefined {
-    return this.options.city
+    return this.options.city;
   }
 
   get address(): string | undefined {
-    return this.options.address
+    return this.options.address;
   }
 
   toString(): string {
-    return `${this.options.longitude},${this.options.latitude}`
+    return `${this.options.longitude},${this.options.latitude}`;
   }
 }
--- src/utils/location/qqMap.ts
+++ src/utils/location/qqMap.ts
@@ -1,33 +1,48 @@
 /*************************
  *       腾讯地图         *
  *************************/
-import Http from 'utils/http'
-import { qqMapKey } from '@appKey'
+import Http from 'utils/http';
+import { qqMapKey } from 'src/appKey';
+
+declare const qq: { maps: { Geolocation: any } };
+
+interface geoResult {
+  module: string;
+  nation: string;
+  province: string;
+  city: string;
+  district: string;
+  adcode: string; // 行政区 ID
+  addr: string;
+  lat: number; // 火星坐标(gcj02), 腾讯、Google、高德通用
+  lng: number;
+  accuracy: number; //误差范围, 以米为单位
+}
 
 class QQMap {
-  private baseUrl: string
-  private http: any
-  private geolocation: any
+  private baseUrl: string;
+  private http: Http;
+  private geolocation;
 
   constructor(private key: string) {
-    this.baseUrl = '/qqmap' // 配合 proxy 实现跨域
+    this.baseUrl = '/qqmap'; // 配合 proxy 实现跨域
 
     this.http = new Http({
       baseUrl: this.baseUrl,
-    })
+    });
 
-    Http.setQQMapInterceptors(this.http.ax)
+    Http.setQQMapInterceptors(this.http.ax);
 
     // 获取定位
-    this.geolocation = new qq.maps.Geolocation(key, '小冰天气')
+    this.geolocation = new qq.maps.Geolocation(this.key, '小冰天气');
   }
 
   // 获取位置信息
   addressInfo() {
     return new Promise((resolve, reject) => {
       this.geolocation.getLocation(
-        (res: any) => {
-          const { lat, lng, city, addr, district, province } = res
+        (res: geoResult) => {
+          const { lat, lng, city, addr, district, province } = res;
           resolve({
             lat, // 纬度
             lng, // 经度
@@ -35,37 +50,54 @@ class QQMap {
             addr, // 具体地址
             district, // 区
             province, // 省
-          })
+          });
         },
-        (err: any) => {
-          reject(err)
+        () => {
+          reject();
         }
-      )
-    })
+      );
+    });
   }
 
   // 获取搜索建议
-  searchSuggestions(keyword: string, region: string = '') {
+  searchSuggestions(keyword: string, region = '') {
     return this.http.request({
       url: '/ws/place/v1/suggestion',
+      method: 'GET',
       data: {
         keyword,
         region,
       },
-    })
+    });
   }
 }
 
 // 导出地图实例
-export const qqMap = new QQMap(qqMapKey)
+export const qqMap = new QQMap(qqMapKey);
 
 /*************************
  *      腾讯地图绘制      *
  *************************/
+interface TMapType {
+  LatLng: any;
+  Map: any;
+  MarkerStyle: any;
+  MultiMarker: any;
+  [key: string]: any;
+}
+
+declare const TMap: TMapType;
+
+// 地图点击事件
+interface event {
+  latLng: { lat: any; lng: any };
+  poi?: { name: string };
+}
+
 export class DrawQQMap {
-  private maker: any
-  private map: any
-  callback: any
+  private maker: TMapType['MultiMarker'];
+  private map: TMapType['Map'];
+  callback: (options: object) => void;
 
   constructor(
     id: number,
@@ -73,45 +105,45 @@ export class DrawQQMap {
     logitude: number,
     callback: (options: object) => void
   ) {
-    this.callback = callback || function () {}
-    this.init(id, latitude, logitude)
+    this.callback = callback || function () {};
+    this.init(id, latitude, logitude);
   }
 
   // 初始化地图
   init(id: number, latitude: number, logitude: number) {
     // 中心点坐标
-    const center = new TMap.LatLng(latitude, logitude)
+    const center = new TMap.LatLng(latitude, logitude);
     // 初始化地图
     this.map = new TMap.Map(id, {
       center,
       zoom: 14, // 缩放比例
-    })
+    });
     // 绑定地图点击事件
-    this.map.on('click', this.select.bind(this))
+    this.map.on('click', this.select.bind(this));
 
     // 获取缩放控件实例
-    const control = this.map.getControl(TMap.constants.DEFAULT_CONTROL_ID.ZOOM)
+    const control = this.map.getControl(TMap.constants.DEFAULT_CONTROL_ID.ZOOM);
     // 设置缩放控件位于右下角
-    control.setPosition(TMap.constants.CONTROL_POSITION.BOTTOM_RIGHT)
+    control.setPosition(TMap.constants.CONTROL_POSITION.BOTTOM_RIGHT);
 
-    this.setMaker(center)
+    this.setMaker(center);
   }
 
   // 点击地图事件
-  select(evt: any) {
-    const { lat, lng } = evt.latLng
-    let name = evt.poi?.name ?? ''
+  select(evt: event) {
+    const { lat, lng } = evt.latLng;
+    const name = evt.poi?.name ?? '';
 
-    this.callback({ latitude: lat, logitude: lng, name }) // 执行回调
+    this.callback({ latitude: lat, logitude: lng, name }); // 执行回调
 
-    this.updateMaker(lat, lng) // 更新锚点
+    this.updateMaker(lat, lng); // 更新锚点
   }
 
   /**
    * @param { TMap.LatLng } loc
    * @returns TMap.MultiMarker instance
    */
-  setMaker(loc: any) {
+  setMaker(loc: TMapType['LatLng']) {
     if (!this.maker) {
       // 设置点标记
       this.maker = new TMap.MultiMarker({
@@ -132,20 +164,20 @@ export class DrawQQMap {
             id: 'marker',
           },
         ],
-      })
+      });
     }
   }
 
   // 更新标记
   updateMaker(latitude: number, longitude: number) {
-    const loc = new TMap.LatLng(latitude, longitude)
+    const loc = new TMap.LatLng(latitude, longitude);
 
     this.maker?.setGeometries([
       {
         position: loc,
         id: 'marker',
       },
-    ])
-    this.map?.setCenter(loc)
+    ]);
+    this.map?.setCenter(loc);
   }
 }
