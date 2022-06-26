0505713379cf905bfddb519f5f16f124db18cd04
ARCTURUS <1638330246@qq.com>
2022-06-26 18:11:31 +0800

    feat: add some translations

--- src/i18n/zhHans.ts
+++ src/i18n/zhHans.ts
@@ -1,3 +1,32 @@
 export default {
-  test: '测试',
+  project: '小冰天气',
+  weather: {
+    wind: '风速',
+    pressure: '大气压',
+    uv: '紫外线指数',
+    precipitation: '降水量',
+    precipChance: '降水概率',
+    temperature: '温度',
+  },
+  date: {
+    morning: '早晨',
+    afternoon: '中午',
+    evening: '旁晚',
+    night: '夜间',
+  },
+  setting: '设置',
+  map: '地图',
+  favorites: '收藏',
+  timestamp: '时间戳',
+  dashBoard: '仪表盘',
+  search: '搜索',
+  account: {
+    logOut: '退出',
+    logIn: '登录',
+    signIn: '注册',
+    forget: '忘记密码',
+  },
+  waring: {
+    key: 'key 缺失',
+  },
 };
