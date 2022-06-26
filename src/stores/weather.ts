fd1bd030d7734289fef32b1762d68cf03e8f936d
ARCTURUS <1638330246@qq.com>
2022-06-26 19:34:42 +0800

    feat: 天气信息状态管理

--- src/stores/weather.ts
+++ src/stores/weather.ts
@@ -1,15 +1,42 @@
 import { defineStore } from 'pinia';
+import Location from 'utils/location/location';
+import QWeatherStrategies from 'utils/weather/strategies/qweather';
+import Weather from 'utils/weather/strategies/weather';
 
-export const useCounterStore = defineStore('counter', {
-  state: () => ({
-    counter: 0,
+interface data {
+  location: Location;
+  current?: IWeather;
+  local?: IWeather;
+  startegies: string;
+  weather: Weather;
+}
+
+const qweather = new QWeatherStrategies(process.env.qWeatherKey!);
+
+export const useWeatherStore = defineStore('weather', {
+  state: (): data => ({
+    location: new Location({
+      latitude: 39.9,
+      longitude: 116.38,
+      city: '北京市',
+      address: '天安门',
+    }),
+    startegies: 'qWeather', // 当前策略
+    weather: new Weather(qweather, 'qWeather'),
+    current: undefined,
+    local: undefined,
   }),
-  getters: {
-    doubleCount: (state) => state.counter * 2,
-  },
+  getters: {},
   actions: {
-    increment() {
-      this.counter++;
+    getAllWeather() {
+      this.weather
+        .getAllweather(this.location as Location)
+        .then((res: IWeather | void) => {
+          console.log(res);
+          if (typeof res !== 'undefined') {
+            this.current = res;
+          }
+        });
     },
   },
 });
