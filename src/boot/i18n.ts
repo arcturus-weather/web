9b3573e99f44b3c9ae41918b2568e037d3e278f5
ARCTURUS <1638330246@qq.com>
2022-06-29 00:44:39 +0800

    feat: i18n 在组合式API中使用

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -4,9 +4,11 @@ import { createI18n } from 'vue-i18n';
 import messages from 'src/i18n';
 
 const i18n = createI18n({
-  locale: 'zhHans',
+  locale: 'zh-CN',
+  fallbackLocale: 'zh-CN',
   messages,
   globalInjection: true,
+  legacy: false,
 });
 
 export default boot(({ app }) => {
