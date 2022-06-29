19e66898dccf23c23320828c9591c5196d78c6ac
ARCTURUS <1638330246@qq.com>
2022-06-29 17:40:44 +0800

    feat: 首页新增地图选点组件

--- src/pages/dashBoard.vue
+++ src/pages/dashBoard.vue
@@ -1,12 +1,31 @@
 <template>
-  <q-page class="row items-center justify-evenly"> hello </q-page>
+  <q-page class="row items-center justify-evenly">
+    <ice-map v-model="visible" @confirm="confirm"></ice-map>
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab icon="search" color="primary" @click="visible = true" />
+    </q-page-sticky>
+  </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import iceMap from 'src/components/ice-map.vue';
 
 export default defineComponent({
   name: 'dashBoardPage',
-  setup() {},
+
+  components: { iceMap },
+
+  methods: {
+    confirm(e: { latitude: number; longitude: number }) {
+      console.log(e);
+    },
+  },
+
+  setup() {
+    return {
+      visible: ref(false),
+    };
+  },
 });
 </script>
