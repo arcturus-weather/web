bcfa6cae5a444658173bae43e9e929cc2195bb85
ARCTURUS <1638330246@qq.com>
2022-06-30 12:13:06 +0800

    feat: none

--- src/i18n/zh-CN.ts
+++ src/i18n/zh-CN.ts
@@ -1,5 +1,11 @@
 export default {
   project: '小冰天气',
+  date: {
+    morning: '早晨',
+    afternoon: '中午',
+    evening: '旁晚',
+    night: '夜间',
+  },
   weather: {
     wind: '风速',
     pressure: '大气压',
@@ -7,12 +13,10 @@ export default {
     precipitation: '降水量',
     precipChance: '降水概率',
     temperature: '温度',
-  },
-  date: {
-    morning: '早晨',
-    afternoon: '中午',
-    evening: '旁晚',
-    night: '夜间',
+    AQI: '空气质量指数',
+    astronomy: '日月升落',
+    hourly: '小时概述',
+    daily: '逐日概述',
   },
   github: '项目地址',
   issue: '问题/建议',
