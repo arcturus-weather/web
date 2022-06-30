d6d7af80fdc40bb80064bacbf1da8d2536dfe0f1
ARCTURUS <1638330246@qq.com>
2022-06-30 18:03:34 +0800

    feat: add a hashMap about pollutions

--- src/utils/weather/tools.ts
+++ src/utils/weather/tools.ts
@@ -6,3 +6,51 @@ export function aqiLevel(aqi: number): number {
   else if (aqi > 200 && aqi <= 300) return 5;
   else return 6;
 }
+
+const aqiMap = {
+  1: 'excellent',
+  2: 'good',
+  3: 'lightlyPollution',
+  4: 'ModeratePollution',
+  5: 'HeavyPollution',
+  6: 'SeriousPollution',
+};
+
+export function aqiCategory(aqi: number): string {
+  return aqiMap[aqiLevel(aqi)];
+}
+
+export const pollutionsMap = {
+  co: {
+    label: 'CO',
+    max: 1500,
+  },
+  no: {
+    label: 'NO',
+    max: 0.69,
+  },
+  no2: {
+    label: 'NO₂',
+    max: 3840,
+  },
+  o3: {
+    label: 'O₃',
+    max: 1200,
+  },
+  so2: {
+    label: 'SO₂',
+    max: 800,
+  },
+  pm2p5: {
+    label: 'PM2.5',
+    max: 500,
+  },
+  pm10: {
+    label: 'PM10',
+    max: 600,
+  },
+  nh3: {
+    label: 'NH₃',
+    max: 0.69,
+  },
+};
