e3a0bbbf12674303d152e42dab153e1047d2e10f
ARCTURUS <1638330246@qq.com>
2022-07-01 15:05:18 +0800

    feat: [components] hourly

---
+++ src/components/ice-hourly-panel.vue
@@ -0,0 +1,156 @@
+<template>
+  <q-dialog
+    :v-model="visible"
+    @update:model-value="(value) => $emit('update:model-value', value)"
+  >
+    <q-card>
+      <!-- date -->
+      <q-card-section class="row items-center q-pb-none">
+        <div class="text-h6">{{ $d(hour!.dateTime, 'long') }}</div>
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+
+      <!-- detail info -->
+      <q-card-section
+        class="column items-center"
+        style="width: 350px;"
+      >
+        <div class="column justify-center items-center q-mb-md">
+          <i-icon :name="hour!.icon" :size="70"></i-icon>
+          <div>{{ hour!.description }}, {{ hour!.temperature.day }}°</div>
+        </div>
+        <div class="grid">
+          <div class="column justify-center items-center">
+            <q-icon name="air" size="20px"></q-icon>
+            <div>{{ hour!.wind.windDir }}</div>
+            <div>
+              {{ hour!.wind.windSpeed }}km/h / {{ hour!.wind.windScale }}
+            </div>
+          </div>
+          <div
+            class="column justify-center items-center"
+            v-for="(item, idx) in other"
+            :key="idx"
+          >
+            <q-icon :name="item.icon" size="20px"></q-icon>
+            <div>{{ $t(`weather.${item.label}`) }}</div>
+            <div>{{ item.value }}</div>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useWeatherStore } from 'src/stores/stores';
+import { storeToRefs } from 'pinia';
+
+const { current } = storeToRefs(useWeatherStore());
+
+interface Other {
+  label: string;
+  value: number | string;
+  icon: string;
+}
+
+export default defineComponent({
+  name: 'iceHourlyPanel',
+
+  props: {
+    visible: {
+      default: false,
+      type: Boolean,
+    },
+    // 这里也可以直接传个对象过来
+    // 但好像直接使用 idx 获取全局状态会更快?
+    idx: Number,
+  },
+
+  emits: ['update:model-value'],
+
+  computed: {
+    hour() {
+      return current!.value?.hourly[this.idx];
+    },
+
+    other() {
+      const h = this.hour as IWeatherItem;
+      const res: Other[] = [
+        {
+          label: 'dew',
+          value: `${h.dewPoint}°`,
+          icon: 'mdi-thermometer-lines',
+          // icon: 'mdi-sun-thermometer-outline',
+        },
+        {
+          label: 'humidity',
+          value: `${h.humidity}%`,
+          icon: 'water_drop',
+        },
+        {
+          label: 'pressure',
+          value: h.pressure,
+          icon: 'speed',
+        },
+      ];
+
+      if (h.clouds) {
+        res.push({
+          label: 'coluds',
+          value: h.clouds,
+          icon: 'cloud',
+        });
+      }
+
+      if (h.uvIndex) {
+        res.push({
+          label: 'uvIndex',
+          value: h.uvIndex,
+          icon: 'beach_access',
+        });
+      }
+
+      if (h.visibility) {
+        res.push({
+          label: 'visibility',
+          value: h.visibility,
+          icon: 'visibility',
+        });
+      }
+
+      if (typeof h.pop !== 'undefined') {
+        res.push({
+          label: 'precipChance',
+          value: `${h.pop}%`,
+          icon: 'mdi-weather-rainy',
+        });
+      }
+
+      if (typeof h.precip !== 'undefined') {
+        res.push({
+          label: 'precipitation',
+          value: `${h.precip}mm`,
+          icon: 'mdi-weather-pouring',
+        });
+      }
+
+      return res;
+    },
+  },
+
+  setup() {
+    return { current };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
+}
+</style>
---
+++ src/components/ice-hourly.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-scroll-area
+    style="height: 90px"
+    :thumb-style="{
+      bottom: '2px',
+      borderRadius: '5px',
+      opacity: '0.1',
+    }"
+  >
+    <ice-transition>
+      <q-list v-if="visible" class="row no-wrap" style="height: 90px">
+        <q-item
+          v-for="(item, idx) in current!.hourly"
+          :key="idx"
+          clickable
+          v-ripple
+          @click="openHourlyPanel(idx)"
+          class="margin-right column justify-between items-center q-px-xs"
+          style="width: 60px"
+        >
+          <q-item-label class="text-center">
+            {{ $d(item.dateTime, 'time3') }}
+          </q-item-label>
+          <q-item-section>
+            <i-icon :name="item.icon" :size="30"></i-icon>
+          </q-item-section>
+          <q-item-label class="text-center text-bold">
+            {{ item.temperature.day }}°
+          </q-item-label>
+        </q-item>
+      </q-list>
+      <div v-else class="row no-wrap">
+        <q-skeleton
+          class="margin-right"
+          width="60px"
+          height="90px"
+          v-for="i in 8"
+          :key="i"
+        />
+      </div>
+    </ice-transition>
+  </q-scroll-area>
+  <ice-hourly-panel v-model="open" :idx="idx"></ice-hourly-panel>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import iceTransition from 'components/ice-transition.vue';
+import iceHourlyPanel from 'components/ice-hourly-panel.vue';
+import { useWeatherStore } from 'stores/stores';
+import { storeToRefs } from 'pinia';
+
+export default defineComponent({
+  name: 'iceHourly',
+
+  components: { iceTransition, iceHourlyPanel },
+
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  methods: {
+    openHourlyPanel(e: number) {
+      this.idx = e;
+      this.open = true;
+    },
+  },
+
+  setup() {
+    const { current } = storeToRefs(useWeatherStore()),
+      idx = ref(0),
+      open = ref(false);
+
+    return { current, open, idx };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.margin-right:not(:last-of-type) {
+  margin-right: 10px;
+}
+</style>
