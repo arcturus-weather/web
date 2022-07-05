a9bfea344262fb98f3ab31c3644b6227052550a6
ARCTURUS <1638330246@qq.com>
2022-07-05 12:22:23 +0800

    feat: [components] 新增月相组件

---
+++ src/components/ice-astronomy-panel.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-dialog
+    :v-model="visible"
+    @update:model-value="(value) => $emit('update:model-value', value)"
+  >
+    <q-card style="width: 400px">
+      <q-card-section class="row items-center q-pb-none">
+        <div class="text-h6">{{ $t('weather.moonphase') }}</div>
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+
+      <q-card-section>
+        <q-scroll-area style="height: 350px">
+          <q-timeline color="secondary">
+            <q-timeline-entry
+              v-for="(item, idx) in moonphase"
+              :key="idx"
+              :title="item.name"
+              :subtitle="$d(item.dateTime!, 'long')"
+            >
+              <div class="row items-center justify-between q-pr-md">
+                <div>
+                  <div>
+                    {{ $t('weather.illumination') }} : {{ item.illumination }}
+                  </div>
+                  <div>
+                    {{ $t('weather.moonPhaseValue') }} : {{ item.value }}
+                  </div>
+                </div>
+                <i-icon :name="item.icon"></i-icon>
+              </div>
+            </q-timeline-entry>
+          </q-timeline>
+        </q-scroll-area>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useWeatherStore } from 'src/stores/stores';
+import { storeToRefs } from 'pinia';
+
+const { current } = storeToRefs(useWeatherStore());
+
+export default defineComponent({
+  name: 'iceAstronomyPanel',
+
+  props: {
+    visible: {
+      default: false,
+      type: Boolean,
+    },
+  },
+
+  emits: ['update:model-value'],
+
+  computed: {
+    moonphase() {
+      if (Array.isArray(current!.value!.moon!.moonPhase)) {
+        return current!.value!.moon!.moonPhase;
+      }
+
+      return [];
+    },
+  },
+
+  setup() {
+    return { current };
+  },
+});
+</script>
