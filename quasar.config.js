027f795eea4cb9081cc760e0845d340617096a02
ARCTURUS <1638330246@qq.com>
2022-07-07 15:39:28 +0800

    fix: 腾讯地图定位失败

--- quasar.config.js
+++ quasar.config.js
@@ -104,7 +104,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
-      // https: true
+      https: true,
       open: false, // opens browser window automatically
       proxy: {
         // cross-origin
