b58c4df36dee609d98d8c3d2243f98dacf841432
ARCTURUS <1638330246@qq.com>
2022-07-08 00:47:05 +0800

    refactor: 不再使用 q-drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,33 +1,41 @@
 <template>
   <q-layout>
-    <q-drawer v-model="drawer" show-if-above :width="160" :breakpoint="400">
-      <!-- 侧边栏 -->
-      <div class="sidebar">
-        <!-- 路由 -->
-        <q-tabs v-model="tab" vertical class="text-primary tabs">
-          <q-route-tab
-            no-caps
-            v-for="(item, idx) in _getTab()"
-            :key="idx"
-            :name="item.name"
-            :icon="item.icon"
-            :label="$t(`${item.name}`)"
-            :to="item.url"
-          />
-        </q-tabs>
+    <!-- 导航栏 -->
+    <div class="tabbar" :class="$q.dark.isActive ? 'dark' : 'light'">
+      <q-tabs
+        v-model="tab"
+        :vertical="$q.screen.xs ? false : true"
+        class="text-primary tabs"
+      >
+        <q-route-tab
+          no-caps
+          v-for="(item, idx) in _getTab()"
+          :key="idx"
+          :name="item.name"
+          :icon="item.icon"
+          :label="$t(`${item.name}`)"
+          :to="item.url"
+        />
+      </q-tabs>
 
-        <!-- logo -->
-        <div class="row items-center justify-center absolute-top logo">
-          <q-avatar size="56px" class="q-mr-sm">
-            <img :src="logo" alt="logo" />
-          </q-avatar>
-          <div class="text-weight-bold">{{ $t('appInfo.project') }}</div>
-        </div>
+      <!-- logo -->
+      <div
+        v-if="!$q.screen.xs"
+        class="row items-center justify-center absolute-top logo"
+      >
+        <q-avatar size="56px" class="q-mr-sm">
+          <img :src="logo" alt="logo" />
+        </q-avatar>
+        <div class="text-weight-bold">{{ $t('appInfo.project') }}</div>
       </div>
-    </q-drawer>
+    </div>
+    <!-- 路由 -->
     <q-page-container class="container">
       <router-view v-slot="{ Component }">
-        <ice-transition enter="fadeInUp" leave="fadeOutUp">
+        <ice-transition
+          :enter="$q.screen.xs ? 'fadeInLeft' : 'fadeInUp'"
+          :leave="$q.screen.xs ? 'fadeOutRight' : 'fadeOutUp'"
+        >
           <keep-alive>
             <component :is="Component" />
           </keep-alive>
@@ -84,7 +92,6 @@ export default defineComponent({
 
     return {
       tab: ref('dashBoard'),
-      drawer: ref(true),
       logo: AppInfo.logo,
     };
   },
@@ -94,21 +101,60 @@ export default defineComponent({
 <style lang="scss">
 $logo-height: 64px;
 $logo-margin-bottom: 20px;
+// tabbar 宽高
+$width: 150px;
+$height: 72px;
 
-.sidebar {
-  height: 100%;
+.tabbar {
+  position: fixed;
+  z-index: 9999;
+  backdrop-filter: blur(20px);
+
+  &.dark {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  &.light {
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+
+  @media screen and (min-width: 600px) {
+    // 屏幕宽度大于 500px, 右边导航栏
+    left: 0;
+    top: 0;
+    bottom: 0;
+    width: $width;
+
+    .tabs {
+      padding-top: $logo-margin-bottom + $logo-height;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    // 屏幕宽度小于 500px, 底部导航栏
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: $height;
+  }
 
   .logo {
     height: $logo-height;
     margin-bottom: $logo-margin-bottom;
   }
-
-  .tabs {
-    padding-top: $logo-margin-bottom + $logo-height;
-  }
 }
 
 .container {
   overflow: hidden;
+  box-sizing: border-box;
+  height: 100vh;
+
+  @media screen and (min-width: 600px) {
+    padding-left: $width;
+  }
+
+  @media screen and (max-width: 600px) {
+    padding-bottom: $height;
+  }
 }
 </style>
