0259cc080b0b6da6a61f26a14f52aac3df782ecb
ARCTURUS <1638330246@qq.com>
2022-07-08 18:28:54 +0800

    feat: 适配移动端

--- src/pages/dashBoard.vue
+++ src/pages/dashBoard.vue
@@ -1,41 +1,28 @@
 <template>
   <q-page>
-    <div class="row skeleton__container">
-      <div class="col-9 column height__100 q-pa-md">
-        <div class="row item-stretch">
-          <!-- main -->
-          <div class="col-8 q-pr-xs">
-            <ice-main :visible="ready" @open-map="openMap = true"></ice-main>
-          </div>
-          <!-- aqi panel -->
-          <div class="col-4">
-            <ice-air :visible="ready"></ice-air>
-          </div>
-          <!-- sun and moon -->
-          <div class="col-4 q-mt-md">
-            <ice-astronomy :visible="ready"></ice-astronomy>
-          </div>
-          <!-- 风速,气压等 -->
-          <div class="col-8 q-mt-md q-pl-xs gutter">
-            <div v-for="i in 4" :key="i">
-              <q-card flat bordered style="height: 100%">
-                <q-card-section>
-                  <q-skeleton height="80px"></q-skeleton>
-                </q-card-section>
-              </q-card>
-            </div>
-          </div>
+    <q-scroll-area style="height: 100vh" :thumb-style="{ width: '0px' }">
+      <div class="row items-stretch home-page__container">
+        <!-- main -->
+        <div class="col-xs-12 col-sm-6 col-md-9 q-pa-sm">
+          <ice-main :visible="ready" @open-map="openMap = true"></ice-main>
         </div>
-      </div>
-      <!-- weather data -->
-      <div class="col-3 row">
-        <q-separator vertical class="height__100" />
-        <div class="column height__100 width__100 justify-between q-pa-md">
+        <!-- aqi panel -->
+        <div class="col-xs-12 col-sm-6 col-md-3 q-pa-sm card">
+          <ice-air :visible="ready"></ice-air>
+        </div>
+        <!-- 小时概述 -->
+        <div class="col-xs-12 col-sm-12 col-md-9 q-pa-sm card">
           <ice-hourly :visible="ready"></ice-hourly>
-          <ice-daily :visible="ready"></ice-daily>
         </div>
+        <!-- 日月升落 -->
+        <div class="col-xs-12 col-sm-6 col-md-3 q-pa-sm card">
+          <ice-astronomy :visible="ready"></ice-astronomy>
+        </div>
+        <!-- <div>
+          <ice-daily :visible="ready"></ice-daily>
+        </div> -->
       </div>
-    </div>
+    </q-scroll-area>
 
     <!-- 底部悬浮按钮 -->
     <q-page-sticky
@@ -64,7 +51,7 @@ import iceMain from 'components/ice-main.vue';
 import iceAir from 'components/ice-air.vue';
 import iceAstronomy from 'components/ice-astronomy.vue';
 import iceHourly from 'components/ice-hourly.vue';
-import iceDaily from 'components/ice-daily.vue';
+// import iceDaily from 'components/ice-daily.vue';
 import { storeToRefs } from 'pinia';
 import { defineComponent, ref } from 'vue';
 import iceMap from 'src/components/ice-map.vue';
@@ -75,7 +62,14 @@ const location = useLocationStore();
 export default defineComponent({
   name: 'dashBoardPage',
 
-  components: { iceMap, iceMain, iceAir, iceAstronomy, iceHourly, iceDaily },
+  components: {
+    iceMap,
+    iceMain,
+    iceAir,
+    iceAstronomy,
+    iceHourly,
+    /*iceDaily,*/
+  },
 
   methods: {
     confirm(e: IMapData) {
@@ -87,8 +81,8 @@ export default defineComponent({
     const { ready } = storeToRefs(useWeatherStore());
     const { current: loc } = storeToRefs(location);
 
-    // location.getLocation();
-    useWeatherStore().getAllWeather();
+    location.getLocation();
+    // useWeatherStore().getAllWeather();
 
     return { ready, openMap: ref(false), loc };
   },
@@ -96,19 +90,16 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.skeleton__container {
-  .height__100 {
-    height: 100%;
+.home-page__container {
+  box-sizing: border-box;
+
+  @media screen and (max-width: 600px) {
+    padding-bottom: 72px; // 这是 tabbar 高度
   }
 
-  .width__100 {
-    width: calc(100% - 1px);
-  }
-
-  .gutter {
-    display: grid;
-    grid-gap: 4px;
-    grid-template-columns: repeat(2, 1fr);
+  .card {
+    height: 300px;
+    box-sizing: border-box;
   }
 }
 </style>
