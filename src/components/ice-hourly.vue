9ca50952437aad7ea9ce971fe43cfb5664256a9c
ARCTURUS <1638330246@qq.com>
2022-07-08 18:21:44 +0800

    refactor: 重新调整布局, 加入折线图

--- src/components/ice-hourly.vue
+++ src/components/ice-hourly.vue
@@ -1,54 +1,145 @@
 <template>
-  <q-scroll-area
-    style="height: 90px"
-    :thumb-style="{
-      bottom: '2px',
-      borderRadius: '5px',
-      opacity: '0.1',
-    }"
-  >
-    <ice-transition>
-      <q-list v-if="visible" class="row no-wrap" style="height: 90px">
-        <q-item
-          v-for="(item, idx) in current!.hourly"
-          :key="idx"
-          clickable
-          v-ripple
-          @click="openHourlyPanel(idx)"
-          class="margin-right column justify-between items-center q-px-xs"
-          style="width: 60px; position: relative"
-        >
-          <q-item-label class="text-center">
-            {{ $d(item.dateTime, 'time3') }}
-          </q-item-label>
-          <q-item-section>
-            <i-icon :name="item.icon" :size="30"></i-icon>
-          </q-item-section>
-          <q-item-label class="text-center text-bold">
-            {{ item.temperature.day }}°
-          </q-item-label>
-        </q-item>
-      </q-list>
-      <div v-else class="row no-wrap">
-        <q-skeleton
-          class="margin-right"
-          width="60px"
-          height="90px"
-          v-for="i in 8"
-          :key="i"
-        />
-      </div>
-    </ice-transition>
-  </q-scroll-area>
+  <q-card flat bordered style="height: 100%">
+    <q-card-section class="text-bold">
+      {{ $t('weather.hourly') }}</q-card-section
+    >
+    <q-scroll-area
+      class="height"
+      :thumb-style="{
+        bottom: '2px',
+        borderRadius: '5px',
+        opacity: '0.1',
+      }"
+    >
+      <ice-transition>
+        <q-list v-if="visible" class="row no-wrap height list">
+          <q-item
+            v-for="(item, idx) in current!.hourly"
+            :key="idx"
+            clickable
+            v-ripple
+            @click="openHourlyPanel(idx)"
+            class="list-item column justify-between items-center q-px-xs"
+          >
+            <!-- 时间 -->
+            <q-item-label class="text-center">
+              {{ $d(item.dateTime, 'hour') }}
+            </q-item-label>
+            <!-- 图标 -->
+            <q-item-section>
+              <i-icon :name="item.icon" :size="40"></i-icon>
+              <div class="text-center">{{ item.description }}</div>
+            </q-item-section>
+          </q-item>
+
+          <!-- 折线图 -->
+          <div ref="hour" class="graph"></div>
+        </q-list>
+        <!-- 骨架屏 -->
+        <div v-else class="row no-wrap">
+          <q-skeleton
+            class="margin-right height"
+            width="60px"
+            v-for="i in 20"
+            :key="i"
+          />
+        </div>
+      </ice-transition>
+    </q-scroll-area>
+  </q-card>
   <ice-hourly-panel v-model="open" :idx="idx"></ice-hourly-panel>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { date } from 'quasar';
+import { defineComponent, ref, watchEffect } from 'vue';
 import iceTransition from 'components/ice-transition.vue';
 import iceHourlyPanel from 'components/ice-hourly-panel.vue';
 import { useWeatherStore } from 'stores/stores';
 import { storeToRefs } from 'pinia';
+import { DualAxes } from '@antv/g2plot';
+
+interface TData {
+  x: string;
+  pop: number;
+  temp: number;
+}
+
+// 绘制温度折线图
+function createGraph(dom: HTMLDivElement | null | string, data: TData[]) {
+  console.log(data);
+  const dualAxes = new DualAxes(dom ?? 'hour', {
+    data: [data, data],
+    legend: false, // 关闭图例
+    xField: 'x',
+    yField: ['temp', 'pop'],
+    yAxis: {
+      pop: {
+        label: null,
+        line: null,
+        max: 100,
+      },
+      temp: {
+        range: [0.2, 0.8],
+        tickCount: 3,
+        tickInterval: 10,
+        grid: {
+          line: {
+            style: {
+              opacity: 0.5,
+            },
+          },
+        },
+      },
+    },
+    xAxis: {
+      range: [0.01, 0.98],
+      label: null,
+      line: null,
+    },
+    renderer: 'svg',
+    autoFit: true,
+    label: {
+      offset: 15,
+      formatter(text) {
+        return `${text.value}°`;
+      },
+    },
+    geometryOptions: [
+      {
+        geometry: 'line',
+        lineStyle: {
+          lineWidth: 2,
+        },
+        point: {
+          color: '#F56C6C',
+        },
+        label: {
+          formatter(v) {
+            return `${v.temp}°`;
+          },
+        },
+      },
+      {
+        // 柱形图
+        geometry: 'column',
+        label: {
+          formatter(v) {
+            return `${v.pop}%`;
+          },
+          offset: 10,
+        },
+        columnStyle: {
+          fill: '#a0cfff',
+          fillOpacity: 0.5,
+        },
+        columnWidthRatio: 0.1,
+      },
+    ],
+  });
+
+  dualAxes.render();
+}
 
 export default defineComponent({
   name: 'iceHourly',
@@ -72,23 +163,47 @@ export default defineComponent({
   setup() {
     const { current } = storeToRefs(useWeatherStore()),
       idx = ref(0),
-      open = ref(false);
+      open = ref(false),
+      hour = ref<HTMLDivElement | null>(null);
 
-    return { current, open, idx };
+    watchEffect(() => {
+      if (hour.value) {
+        createGraph(
+          hour.value,
+          current.value!.hourly.map((e: IWeatherItem) => ({
+            x: date.formatDate(e.dateTime, 'HH:mm'),
+            temp: e.temperature.day!,
+            pop: e.pop!,
+          }))
+        );
+      }
+    });
+
+    return { current, open, idx, hour };
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.margin-right:not(:last-of-type) {
-  margin-right: 10px;
+.height {
+  height: 230px; // list 高度
 }
 
-.per {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+.list {
+  $height: 90px; // 折线图高度
+  position: relative;
+
+  .graph {
+    height: $height;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    z-index: -1;
+  }
+
+  .list-item {
+    width: 60px;
+    padding-bottom: $height;
+  }
 }
 </style>
