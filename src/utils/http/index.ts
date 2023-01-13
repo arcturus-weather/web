c5edbbbb08fd932ee6532610d98224df2917036e
ARCTURUS <1638330246@qq.com>
2023-01-14 00:39:48 +0800

    refactor: 删除不必要代码

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -1,4 +1,3 @@
-import { qWeatherCode, qqMapCode } from './code';
 import axios, {
   AxiosInstance,
   AxiosRequestConfig,
@@ -75,40 +74,6 @@ export default class Http {
         return Promise.reject(err.response.data);
       }
     );
-  }
-
-  // qWeather
-  static setQweatherResponseInterceptors(ax: AxiosInstance) {
-    Http.setResponseInterceptors(ax, (resp) => {
-      const res = resp.data;
-      const code = Number(res.code);
-
-      if (code === 200) {
-        return Promise.resolve(res);
-      } else {
-        notify.negative(qWeatherCode[code as keyof typeof qWeatherCode]);
-        return Promise.reject();
-      }
-    });
-  }
-
-  // tencent map
-  static setQQMapResponseInterceptors(ax: AxiosInstance) {
-    Http.setResponseInterceptors(ax, (resp) => {
-      const data = resp.data;
-      const status = data.status as number;
-
-      if (status === 0) {
-        if (typeof data.data !== 'undefined') {
-          return Promise.resolve(data.data);
-        } else if (typeof data.result !== 'undefined') {
-          return Promise.resolve(data.result);
-        } else return data;
-      } else {
-        notify.negative(qqMapCode[status as keyof typeof qqMapCode]);
-        return Promise.reject();
-      }
-    });
   }
 }
 
