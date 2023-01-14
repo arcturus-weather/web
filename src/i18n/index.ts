dc4417942f89d0dfeaa64be20d92998d75844a9c
ARCTURUS <1638330246@qq.com>
2023-01-14 11:13:42 +0800

    feat: add en-GB

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,6 +4,7 @@ import zhTW, { time as TW_Time } from './zh-TW';
 
 export default {
   'en-US': enUS,
+  'en-GB': enUS,
   'zh-CN': zhCN,
   'zh-TW': zhTW,
 };
@@ -12,4 +13,5 @@ export const datetimeFormats = {
   'zh-TW': TW_Time,
   'zh-CN': CN_Time,
   'en-US': EN_Time,
+  'en-GB': EN_Time,
 };
