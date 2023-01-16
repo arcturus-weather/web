7f0649aaf71071479e217ca815df787d203fe76e
ARCTURUS <1638330246@qq.com>
2023-01-17 00:44:40 +0800

    feat: 更新图标包

--- src/boot/myIcon.ts
+++ src/boot/myIcon.ts
@@ -1,6 +1,6 @@
-import iceWeather from 'iweather_icons';
+import icon from 'iweather_icons';
 import { boot } from 'quasar/wrappers';
 
 export default boot(({ app }) => {
-  app.use(iceWeather);
+  app.use(icon);
 });
